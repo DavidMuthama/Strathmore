48d185953b541fce5194e0c49ee683b07dabb326
DavidMuthama <156419416+DavidMuthama@users.noreply.github.com>
2024-10-08 15:27:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-def install_and_import(package):
-    import importlib
-    try:
-        importlib.import_module(package)
-    except ImportError:
-        import pip
-        pip.main(['install', package])
-    finally:
-        globals()[package] = importlib.import_module(package)
-
-
-install_and_import('plotly')
 # Importing Libraries
 import pandas as pd
 import numpy as np
