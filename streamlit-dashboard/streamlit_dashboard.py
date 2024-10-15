6d701efeb40cef0e759f47e7647d619a7a906aaf
DavidMuthama <156419416+DavidMuthama@users.noreply.github.com>
2024-10-15 14:26:32 +0300

    Update streamlit_dashboard.py

--- streamlit-dashboard/streamlit_dashboard.py
+++ streamlit-dashboard/streamlit_dashboard.py
@@ -13,24 +13,20 @@ df = load_df()
 st.title("Streamlit Dashboard with Plotly")
 st.write("This is a simple example dashboard with Plotly visualizations.")
 
-fig1 = px.line(df, x="year", y="population", color="sex", title="Population Over Time")
+fig1 = px.line(df, x="year", y="total_inactive_population", color="sex",color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Inactive Population By Sex")
 st.plotly_chart(fig1)
 fig2 = px.pie(df, values='total_inactive_population', names='sex', color='sex',
-             color_discrete_map={'male':'blue', 'female':'pink'})
+             color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Inactive Population By Sex")
 st.plotly_chart(fig2)
 
-fig3 = px.line(df, x="year", y="total_inactive_population", color="sex", title="Population Over Time")
+fig3 = px.line(df, x="year", y="total_unemployed_population", color="sex", color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Unemployed Population")
 st.plotly_chart(fig3)
-fig4 = px.pie(df,values='total_inactive_population',color="sex", title="Population Over Time")
+fig4 = px.pie(df, values='total_unemployed_population', names='sex', color='sex',
+             color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Unemployed Population")
 st.plotly_chart(fig4)
 
-fig5 = px.line(df, x="year", y="total_unemployed_population", color="sex", title="Population Over Time")
+fig5 = px.line(df, x="year", y="total_employed_population", color="sex", color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Employed Population")
 st.plotly_chart(fig5)
-fig6 = px.pie(df,color="sex", title="Population Over Time")
+fig6 = px.pie(df, values='total_employed_population', names='sex', color='sex',
+             color_discrete_map={'male':'blue', 'female':'pink'}, title="Total Employed Population")
 st.plotly_chart(fig6)
-
-fig7 = px.line(df, x="year", y="total_employed_population", color="sex", title="Population Over Time")
-st.plotly_chart(fig7)
-
-fig8 = px.pie(df,color="sex", title="Population Over Time")
-st.plotly_chart(fig8)
